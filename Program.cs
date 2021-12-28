384f95136b0906d277d58abbd4cc7f3c436d4a41
ZaidandHussein <95748216+ZaidandHussein@users.noreply.github.com>
2021-12-28 08:47:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Telegram.Bot;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static string token = "5054431987:AAHda-dOE8keKUqrJk9n8UpxOHSoz3EopM8";
+        static Telegram.Bot.TelegramBotClient bot = new TelegramBotClient(token);
+        static void  Main(string[] args)
+        {
+            HttpClient clint = new HttpClient();
+            int offset = 0;
+            while(true)
+            {
+                Telegram.Bot.Types.Update[] updates = bot.GetUpdatesAsync(offset).Result;
+                try
+                {
+                    foreach (var update in updates)
+                    {
+
+                        offset = update.Id + 1;
+                        if (update.Message != null)
+                        {
+                            long id = update.Message.From.Id;
+                            string txt = update.Message.Text;
+                            string username = update.Message.From.Username;
+                            string firstname = update.Message.From.FirstName;
+                            string lastname = update.Message.From.LastName;
+
+                            if (txt == "/start")
+                            {
+                                bot.SendTextMessageAsync(id, "You are welcome to know the weather please write the name of the city");
+                                Console.WriteLine(id);
+                                Console.WriteLine(txt);
+                                Console.WriteLine(username);
+                                Console.WriteLine(firstname);
+                                Console.WriteLine(lastname);
+                            }
+                            else
+                            {
+                                var x = "http://api.openweathermap.org/data/2.5/weather?q=";
+                                var responseTask = clint.GetAsync(x + txt + "&units=metric&APPID=c9143c3f71a3053bf120412bd12e286c");
+                                responseTask.Wait();
+
+                                if (responseTask.IsCompleted)
+                                {
+                                    var result = responseTask.Result;
+                                    if (result.IsSuccessStatusCode)
+                                    {
+
+                                        var massage = result.Content.ReadAsStringAsync();
+
+                                        massage.Wait();
+                                        Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(massage.Result);
+                                       
+
+                                        bot.SendTextMessageAsync(id, "temperature.value: " + Convert.ToString( myDeserializedClass.main.temp ));
+                                        bot.SendTextMessageAsync(id, "temperature.min: "+ Convert.ToString(myDeserializedClass.main.temp_min ));
+                                        bot.SendTextMessageAsync(id, "temperature.max: " + Convert.ToString(myDeserializedClass.main.temp_max ));
+                                      
+                                       
+                                    }
+                                }
+
+
+                            }
+                        }
+
+                    }
+                }
+
+                catch (Exception)
+                {
+
+
+                }
+                
+            }
+        }
+    }
+}
